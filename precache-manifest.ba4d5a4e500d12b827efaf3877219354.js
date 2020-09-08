9cc0de8bf8d2ff5b1dc5e1a7dec912fdb28a965f
volder <vlodyk00@gmail.com>
2020-09-08 19:02:14 +0300

    Updates

---
+++ precache-manifest.ba4d5a4e500d12b827efaf3877219354.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16530799e79a9e125ecc7819ae961a36",
+    "url": "/react-firebase-todolist/index.html"
+  },
+  {
+    "revision": "2a09ce4958bb3bf2a27a",
+    "url": "/react-firebase-todolist/static/css/main.12480f45.chunk.css"
+  },
+  {
+    "revision": "510bbf2df4459f149f88",
+    "url": "/react-firebase-todolist/static/js/2.8ca242ff.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/react-firebase-todolist/static/js/2.8ca242ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a09ce4958bb3bf2a27a",
+    "url": "/react-firebase-todolist/static/js/main.56daef8c.chunk.js"
+  },
+  {
+    "revision": "a9aedf03a62982f9ab4d",
+    "url": "/react-firebase-todolist/static/js/runtime-main.79bfa9ba.js"
+  },
+  {
+    "revision": "706d097549c1ee86d3327a6e9f18a2e0",
+    "url": "/react-firebase-todolist/static/media/add.706d0975.svg"
+  },
+  {
+    "revision": "333a7a4a266c7487d5153a6cf3409027",
+    "url": "/react-firebase-todolist/static/media/check.333a7a4a.svg"
+  }
+]);
